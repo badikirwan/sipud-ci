91c26ed1bd380fa1def1105ce39bfdb223883998
badikirwan <badikirwan@gmail.com>
2017-05-24 04:46:45 +0700

    Versi 1.1

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+		$data = array(
+			'contents' 	=> 'dashboard' ,
+			'penduduk'  => '',
+      'master'    => '',
+			'title'			=> 'SIPUD',
+			'menu'			=> 'menu.php'
+		);
+		$this->load->view('index', $data);
+	}
+
+	public function agama()
+	{
+			$data['active'] = 'active';
+			$data['contents'] = 'agama/agama';
+			$this->load->view('index', $data);
+	}
+}
